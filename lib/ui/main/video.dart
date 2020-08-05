aacd6bed0a1d5aa4fd7d6e562552d19153da5104
Jimmy Kumpulainen <jimmy@kumpulainen.se>
2020-08-05 17:17:36 +0200

    Changed video player

--- lib/ui/main/video.dart
+++ lib/ui/main/video.dart
@@ -1,11 +1,10 @@
-import 'package:auto_orientation/auto_orientation.dart';
 import 'package:bomb_watch/data/api_responses/gb_video.dart';
 import 'package:bomb_watch/services/gb_client.dart';
-import 'package:chewie/chewie.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get_it/get_it.dart';
+import 'package:neeko/neeko.dart';
 import 'package:video_player/video_player.dart';
 
 class VideoScreen extends StatefulWidget {
@@ -18,135 +17,80 @@ class VideoScreen extends StatefulWidget {
 }
 
 class _VideoScreenState extends State<VideoScreen> {
+  final String guid;
+
+  bool hasLoaded = false;
+
+  Future<GbVideo> futureVideo;
   GbClient _gbClient = GetIt.instance<GbClient>();
 
-  TargetPlatform _platform;
-  VideoPlayerController _videoPlayerController;
-  ChewieController _chewieController;
-  Chewie _playerWidget;
+  // VideoPlayerController _videoPlayerController;
 
-  String guid;
-  Future<GbVideo> futureVideo;
+  VideoControllerWrapper _videoControllerWrapper;
 
   _VideoScreenState(this.guid);
 
   @override
   void initState() {
     super.initState();
+    SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.top]);
     futureVideo = _gbClient.fetchVideo(this.guid);
     futureVideo.then((video) {
-      _videoPlayerController = VideoPlayerController.network(
-          "${video.results.hdUrl}?api_key=${_gbClient.apiKey}")
-        ..initialize().then((_) {
-          // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
-          setState(() {});
+      _videoControllerWrapper = VideoControllerWrapper(DataSource.network(
+          "${video.results.hdUrl}?api_key=${_gbClient.apiKey}",
+          displayName: video.results.name));
+
+      _videoControllerWrapper.addListener(() {
+        setState(() {
+          hasLoaded = true;
         });
-
-      _chewieController = ChewieController(
-          videoPlayerController: _videoPlayerController,
-          // aspectRatio: 3 / 2,
-          //_videoPlayerController.value.aspectRatio,
-          autoPlay: false,
-          showControls: true,
-          showControlsOnInitialize: true,
-          looping: false,
-          autoInitialize: false,
-          routePageBuilder: (BuildContext context, Animation<double> animation,
-              Animation<double> secondAnimation, provider) {
-            return AnimatedBuilder(
-              animation: animation,
-              builder: (BuildContext context, Widget child) {
-                return VideoScaffold(
-                  child: Scaffold(
-                    resizeToAvoidBottomPadding: false,
-                    body: Container(
-                      alignment: Alignment.center,
-                      color: Colors.black,
-                      child: provider,
-                    ),
-                  ),
-                );
-              },
-            );
-          });
-
-      _playerWidget = Chewie(controller: _chewieController);
+      });
     });
   }
 
   @override
   void dispose() {
-    if (_videoPlayerController != null) _videoPlayerController.dispose();
-    if (_chewieController != null) _chewieController.dispose();
-
+    // _videoControllerWrapper.dispose();
+    SystemChrome.restoreSystemUIOverlays();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: FutureBuilder<GbVideo>(
-            future: futureVideo,
-            builder: (context, snapshot) {
-              return Scaffold(
-                appBar: AppBar(
-                  title: Text(snapshot.hasData
-                      ? snapshot.data.results?.name
-                      : 'Loading...'),
-                ),
-                body: _getBody(snapshot),
-              );
-            }));
+    return FutureBuilder<GbVideo>(
+        future: futureVideo,
+        builder: (context, snapshot) {
+          return Scaffold(
+            backgroundColor: Colors.black12,
+            appBar: AppBar(
+              title: Text(snapshot.hasData
+                  ? snapshot.data.results?.name
+                  : 'Loading...'),
+            ),
+            body: _getBody(snapshot),
+          );
+        });
   }
 
   _getBody(AsyncSnapshot<GbVideo> snapshot) {
-    if (snapshot.hasData) {
-      return Center(
-
-        child: Chewie(
-          controller: _chewieController,
+    if (snapshot.hasData && hasLoaded) {
+      Video video = snapshot.data.results;
+      return Column(children: <Widget>[
+        NeekoPlayerWidget(
+          videoControllerWrapper: _videoControllerWrapper,
+          playerOptions: NeekoPlayerOptions(autoPlay: false),
         ),
-      );
+        Container(
+            padding: EdgeInsets.all(10),
+            child: Text(
+              'Uploaded by: ${video.user} @ ${video.publishDate}',
+              style: TextStyle(color: Colors.pinkAccent),
+            ))
+      ]);
     } else if (snapshot.hasError) {
       return Text("${snapshot.error}");
     }
 
     return Center(child: CircularProgressIndicator());
-  }
-}
-
-class VideoScaffold extends StatefulWidget {
-  const VideoScaffold({Key key, this.child}) : super(key: key);
-
-  final Widget child;
-
-  @override
-  State<StatefulWidget> createState() => _VideoScaffoldState();
-}
-
-class _VideoScaffoldState extends State<VideoScaffold> {
-  @override
-  void initState() {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.landscapeRight,
-      DeviceOrientation.landscapeLeft,
-    ]);
-    AutoOrientation.landscapeAutoMode();
-    super.initState();
-  }
-
-  @override
-  dispose() {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown,
-    ]);
-    AutoOrientation.portraitAutoMode();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return widget.child;
   }
 }
